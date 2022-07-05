c87f33aa7d60456e0ab7fefcc35c1637d6b25aa3
Silver0034 <airraid.mail@gmail.com>
2022-07-05 14:45:42 -0400

    Update gulpfile and packages

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,44 +2,41 @@
 'use strict'
 
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const cleanCSS = require('gulp-clean-css')
+const fs = require('fs')
+const glob = require('glob')
+const minify = require('gulp-minify')
+const path = require('path')
 const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
-const cleanCSS = require('gulp-clean-css')
-const glob = require('glob')
-const path = require('path')
-const fs = require('fs')
 
 const globs = {
-	input: {
-		admin: './admin/scss/**/*.scss',
-		frontEnd: './front-end/scss/**/*.scss'
+	admin: {
+		styles: {
+			input: './admin/scss',
+			output: './admin/css'
+		},
+		scripts: {
+			input: './admin/js',
+			output: './admin/js'
+		}
 	},
-	output: {
-		admin: './admin/css',
-		frontEnd: './front-end/css'
+	frontEnd: {
+		styles: {
+			input: './front-end/scss',
+			output: './front-end/css'
+		},
+		scripts: {
+			input: './front-end/js',
+			output: './front-end/js'
+		}
 	}
 }
 
-function buildStyles(input, output) {
-	return gulp
-		.src(input)
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(cleanCSS({ compatibility: 'ie8' }))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(output))
-}
-
-function buildAdminStyles(cb) {
-	buildStyles(globs.input.admin, globs.output.admin)
-	cb()
-}
-
-function buildFrontEndStyles(cb) {
-	buildStyles(globs.input.frontEnd, globs.output.frontEnd)
-	cb()
-}
-
+/**
+ * Create a distribution folder for the plugin
+ */
 function buildDist() {
 	// get plugin directory name
 	const directory = path.basename(__dirname)
@@ -84,36 +81,144 @@ function buildDist() {
 	}
 }
 
-gulp.task('build', (cb) => {
+/**
+ * Build styles in input directory and place into output directory
+ * @param string input
+ * @param string output
+ */
+function buildStyles(input, output) {
+	return gulp
+		.src(`${input}/**/*.scss`)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(output))
+}
+
+/**
+ * Build the admin styles
+ * @param function cb
+ */
+function buildAdminStyles(cb) {
+	buildStyles(globs.admin.styles.input, globs.admin.styles.output)
+	cb()
+}
+
+/**
+ * Build the front end styles
+ * @param function cb
+ */
+function buildFrontEndStyles(cb) {
+	buildStyles(globs.frontEnd.styles.input, globs.frontEnd.styles.output)
+	cb()
+}
+
+/**
+ * Build scripts in input directory and place into output directory
+ * @param string input
+ * @param string output
+ */
+function buildScripts(input, output) {
+	return gulp
+		.src([`${input}/**/*.js`, `!${input}/**/*.min.js`])
+		.pipe(
+			babel({
+				presets: ['@babel/env']
+			}).on('error', console.error.bind(console))
+		)
+		.pipe(
+			minify({
+				ext: {
+					min: '.min.js'
+				},
+				noSource: true
+			}).on('error', console.error.bind(console))
+		)
+		.pipe(gulp.dest(output))
+}
+
+/**
+ * Build admin scripts
+ * @param function cb
+ */
+function buildAdminScripts(cb) {
+	buildScripts(globs.admin.scripts.input, globs.admin.scripts.output)
+	cb()
+}
+
+/**
+ * Build front end scripts
+ * @param function cb
+ */
+function buildFrontEndScripts(cb) {
+	buildScripts(globs.frontEnd.scripts.input, globs.frontEnd.scripts.output)
+	cb()
+}
+
+/**
+ * Build admin styles and scripts
+ * @param function cb
+ */
+function buildAdminFiles(cb) {
+	buildStyles(globs.admin.styles.input, globs.admin.styles.output)
+	buildScripts(globs.admin.scripts.input, globs.admin.scripts.output)
+	cb()
+}
+
+/**
+ * Build front end styles and scripts
+ * @param function cb
+ */
+function buildFrontEndFiles(cb) {
+	buildStyles(globs.frontEnd.styles.input, globs.frontEnd.styles.output)
+	buildScripts(globs.frontEnd.scripts.input, globs.frontEnd.scripts.output)
+	cb()
+}
+
+/**
+ * Build the distribution folder
+ * @param function cb
+ */
+gulp.task('dist', (cb) => {
 	buildDist()
 	cb()
 })
 
-gulp.task('admin', (cb) => {
-	buildStyles(globs.input.admin, globs.output.admin)
-	cb()
+// Admin gulp functions
+gulp.task('admin-styles', buildAdminStyles)
+gulp.task('admin-scripts', buildAdminScripts)
+gulp.task('admin', buildAdminFiles)
+gulp.task('watch-admin-styles', () => {
+	gulp.watch(`${globs.admin.styles.input}/**/*.scss`, buildAdminStyles)
 })
-
-gulp.task('front-end', (cb) => {
-	buildStyles(globs.input.frontEnd, globs.output.frontEnd)
-	cb()
+gulp.task('watch-admin-scripts', () => {
+	gulp.watch(`${globs.admin.scripts.input}/**/*.js`, buildAdminScripts)
 })
-
-gulp.task('default', (cb) => {
-	buildStyles(globs.input.admin, globs.output.admin)
-	buildStyles(globs.input.frontEnd, globs.output.frontEnd)
-	cb()
-})
-
 gulp.task('watch-admin', () => {
-	gulp.watch(globs.input.admin, buildAdminStyles)
+	gulp.watch(`${globs.admin.styles.input}/**/*.scss`, buildAdminFiles)
+	gulp.watch(`${globs.admin.scripts.input}/**/*.js`, buildAdminFiles)
 })
 
+// Front end functions
+gulp.task('front-end-styles', buildFrontEndStyles)
+gulp.task('front-end-scripts', buildFrontEndScripts)
+gulp.task('front-end', buildFrontEndFiles)
+gulp.task('watch-front-end-styles', () => {
+	gulp.watch(`${globs.frontEnd.styles.input}/**/*.scss`, buildFrontEndStyles)
+})
+gulp.task('watch-front-end-scripts', () => {
+	gulp.watch(`${globs.frontEnd.scripts.input}/**/*.js`, buildFrontEndScripts)
+})
 gulp.task('watch-front-end', () => {
-	gulp.watch(globs.input.frontEnd, buildFrontEndStyles)
+	gulp.watch(`${globs.frontEnd.styles.input}/**/*.scss`, buildFrontEndFiles)
+	gulp.watch(`${globs.frontEnd.scripts.input}/**/*.js`, buildFrontEndFiles)
 })
 
-gulp.task('watch', () => {
-	gulp.watch(globs.input.admin, buildAdminStyles)
-	gulp.watch(globs.input.frontEnd, buildFrontEndStyles)
+// default
+gulp.task('default', () => {
+	gulp.watch(`${globs.admin.styles.input}/**/*.scss`, buildAdminFiles)
+	gulp.watch(`${globs.admin.scripts.input}/**/*.js`, buildAdminFiles)
+	gulp.watch(`${globs.frontEnd.styles.input}/**/*.scss`, buildFrontEndFiles)
+	gulp.watch(`${globs.frontEnd.scripts.input}/**/*.js`, buildFrontEndFiles)
 })
